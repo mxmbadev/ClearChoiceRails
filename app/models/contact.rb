4ebb9b3f7cb3f071c7901c5fbff668857ac999cf
Brandon Espinoza <espinozab100@gmail.com>
2016-02-04 22:01:50 -0600

    Set up contact form

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message,   :validate => true
+  attribute :nickname,  :captcha  => true
+
+  def headers
+    {
+      :subject => "Clear Choice Contact Form",
+      :to => "espinozabo@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :clients
   resources :cities
+  resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
 end
