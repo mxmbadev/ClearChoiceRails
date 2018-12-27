d11a5d16e3d1d841d2087ec088c381d42c294703
Brandon Espinoza <espinozab100@gmail.com>
2018-12-27 09:43:51 -0600

    fixing syntax for email send to on contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Clear Choice Contact Form",
-      :to => ["espinozab100@gmail.com", "espinozabo@gmail.com"],
+      :to => ['espinozab100@gmail.com', 'espinozabo@gmail.com'],
       :from => %("#{name}" <#{email}>)
     }
   end
